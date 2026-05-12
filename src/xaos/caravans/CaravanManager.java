1480a58d572f8040c36329244e2304e939d19fe1
James <jameslittle12@hotmail.com>
2026-05-12 23:36:11 +0100

    CaravanManager refactor and doc

--- src/xaos/caravans/CaravanManager.java
+++ src/xaos/caravans/CaravanManager.java
@@ -21,29 +21,38 @@ public class CaravanManager {
     private static void loadItems() {
         caravanList = new HashMap<String, CaravanManagerItem>();
 
-        // Cargar de fichero
-        loadXMLEffects(Towns.getPropertiesString("DATA_FOLDER") + "caravans.xml", true); //$NON-NLS-1$ //$NON-NLS-2$
+        String mainCaravanListPath = Towns.getPropertiesString("DATA_FOLDER") + "caravans.xml";
+        loadCaravansFromXml(mainCaravanListPath, true);
 
-        // Mods
-        File fUserFolder = new File(Game.getUserFolder());
-        if (!fUserFolder.exists() || !fUserFolder.isDirectory()) {
+        File userFolder = new File(Game.getUserFolder());
+        if (!userFolder.exists() || !userFolder.isDirectory()) {
             return;
         }
 
-        ArrayList<String> alMods = Game.getModsLoaded();
-        if (alMods != null && alMods.size() > 0) {
-            for (int i = 0; i < alMods.size(); i++) {
-                String sModActionsPath = fUserFolder.getAbsolutePath() + System.getProperty("file.separator") + Game.MODS_FOLDER1 + System.getProperty("file.separator") + alMods.get(i) + System.getProperty("file.separator") + Towns.getPropertiesString("DATA_FOLDER") + "caravans.xml"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
-                File fIni = new File(sModActionsPath);
-                if (fIni.exists()) {
-                    loadXMLEffects(sModActionsPath, false);
-                }
+        ArrayList<String> mods = Game.getModsLoaded();
+        if (mods == null || mods.size() == 0) {
+            return;
+        }
+
+        for (int i = 0; i < mods.size(); i++) {
+            String modCaravanListPath = userFolder.getAbsolutePath()
+                    + System.getProperty("file.separator")
+                    + Game.MODS_FOLDER1
+                    + System.getProperty("file.separator")
+                    + mods.get(i)
+                    + System.getProperty("file.separator")
+                    + Towns.getPropertiesString("DATA_FOLDER")
+                    + "caravans.xml";
+
+            File modCaravanFile = new File(modCaravanListPath);
+            if (modCaravanFile.exists()) {
+                loadCaravansFromXml(modCaravanListPath, false);
             }
         }
     }
 
-    public static CaravanManagerItem getItem(String sIniHeader) {
-        if (sIniHeader == null) {
+    public static CaravanManagerItem getItem(String iniHeader) {
+        if (iniHeader == null) {
             return null;
         }
 
@@ -51,115 +60,133 @@ public class CaravanManager {
             loadItems();
         }
 
-        return caravanList.get(sIniHeader);
+        return caravanList.get(iniHeader);
     }
 
-    private static void loadXMLEffects(String sXMLName, boolean bLoadingMain) {
+    private static void loadCaravansFromXml(String caravanListPathName, boolean isMainFile) {
         try {
-            Document doc = UtilsXML.loadXMLFile(sXMLName); //$NON-NLS-1$
+            Document xmlCaravanList = UtilsXML.loadXMLFile(caravanListPathName);
+            NodeList caravanNodes = xmlCaravanList.getDocumentElement().getChildNodes();
 
-			// Tenemos el documento XML parseado
-            // Lo recorremos entero y vamos añadiendo las caravans a la hash
-            NodeList nodeList = doc.getDocumentElement().getChildNodes();
-            Node node;
-            CaravanManagerItem caravanData;
-            String sID;
-            for (int i = 0; i < nodeList.getLength(); i++) {
-                node = nodeList.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    String sName = node.getNodeName();
-                    if (sName != null && sName.equalsIgnoreCase("DELETE")) {
-                        // Miramos que ID quiere borrar
-                        if (node.getAttributes() != null && node.getAttributes().getNamedItem("id") != null) {
-                            String sIDToDelete = node.getAttributes().getNamedItem("id").getNodeValue();
-                            if (sIDToDelete != null) {
-                                caravanList.remove(sIDToDelete);
-                            }
-                        }
-                        continue;
-                    }
+            for (int i = 0; i < caravanNodes.getLength(); i++) {
+                Node caravanNode = caravanNodes.item(i);
 
-                    sID = node.getNodeName();
-
-                    // Miramos si existe
-                    boolean bExists = caravanList.containsKey(sID);
-                    // Mod cambiando valores de una caravan que ya existe?
-                    boolean bModChangingValues = (bExists && !bLoadingMain);
-
-                    if (bModChangingValues) {
-                        caravanData = caravanList.get(sID);
-
-                        String sAux = UtilsXML.getChildValue(node.getChildNodes(), "zone"); //$NON-NLS-1$
-                        if (sAux != null) {
-                            caravanData.setZone(sAux);
-                        }
-                        sAux = UtilsXML.getChildValue(node.getChildNodes(), "pricePCT"); //$NON-NLS-1$
-                        if (sAux != null) {
-                            caravanData.setPricePCT(sAux);
-                        }
-                        sAux = UtilsXML.getChildValue(node.getChildNodes(), "coins"); //$NON-NLS-1$
-                        if (sAux != null) {
-                            caravanData.setCoins(sAux);
-                        }
-                        sAux = UtilsXML.getChildValue(node.getChildNodes(), "buys"); //$NON-NLS-1$
-                        if (sAux != null) {
-                            caravanData.setBuysString(sAux);
-                        }
-
-                        ArrayList<CaravanItemData> alItems = loadCaravanItems(node.getChildNodes());
-                        if (alItems != null && alItems.size() > 0) {
-                            caravanData.setItemList(alItems);
-                        }
-
-                        sAux = UtilsXML.getChildValue(node.getChildNodes(), "comePCT"); //$NON-NLS-1$
-                        if (sAux != null) {
-                            caravanData.setComePCT(sAux);
-                        }
-                        caravanData.setComePCT(UtilsXML.getChildValue(node.getChildNodes(), "comePCT")); //$NON-NLS-1$
-                    } else {
-                        caravanData = new CaravanManagerItem();
-                        caravanData.setId(sID);
-                        caravanData.setZone(UtilsXML.getChildValue(node.getChildNodes(), "zone")); //$NON-NLS-1$
-                        caravanData.setPricePCT(UtilsXML.getChildValue(node.getChildNodes(), "pricePCT")); //$NON-NLS-1$
-                        caravanData.setCoins(UtilsXML.getChildValue(node.getChildNodes(), "coins")); //$NON-NLS-1$
-                        caravanData.setBuysString(UtilsXML.getChildValue(node.getChildNodes(), "buys")); //$NON-NLS-1$
-                        caravanData.setItemList(loadCaravanItems(node.getChildNodes()));
-                        caravanData.setComePCT(UtilsXML.getChildValue(node.getChildNodes(), "comePCT")); //$NON-NLS-1$
-                    }
-
-                    // Lo añadimos a la hash
-                    caravanList.put(sID, caravanData);
+                if (caravanNode.getNodeType() != Node.ELEMENT_NODE) {
+                    continue;
                 }
+
+                String id = caravanNode.getNodeName();
+
+                if (id.equalsIgnoreCase("DELETE")) {
+                    deleteCaravanFromXml(caravanNode);
+                    continue;
+                }
+
+                CaravanManagerItem caravanData;
+
+                if (!isMainFile && caravanList.containsKey(id)) {
+                    caravanData = caravanList.get(id);
+                    applyOptionalCaravanValues(caravanData, caravanNode);
+                } else {
+                    caravanData = createCaravanFromXml(id, caravanNode);
+                }
+
+                caravanList.put(id, caravanData);
             }
         } catch (Exception e) {
-            Log.log(Log.LEVEL_ERROR, Messages.getString("CaravanManager.0") + e.toString() + "]", "CaravanManager"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+            // message: "Error reading caravans.xml [" + e.toString() + "]"
+            Log.log(Log.LEVEL_ERROR, Messages.getString("CaravanManager.0") + e.toString() + "]", "CaravanManager");
             Game.exit();
         }
     }
 
-    private static ArrayList<CaravanItemData> loadCaravanItems(NodeList nodeList) throws Exception {
-        ArrayList<CaravanItemData> alItemData = new ArrayList<CaravanItemData>();
+    private static CaravanManagerItem createCaravanFromXml(String id, Node caravanNode) throws Exception {
+        CaravanManagerItem caravanData = new CaravanManagerItem();
 
-        Node node;
-        for (int i = 0; i < nodeList.getLength(); i++) {
-            node = nodeList.item(i);
-            if (node.getNodeType() == Node.ELEMENT_NODE) {
-                if (node.getNodeName().equals("item")) { //$NON-NLS-1$
-                    CaravanItemData cid = new CaravanItemData();
-                    cid.setItemId(UtilsXML.getChildValue(node.getChildNodes(), "id")); //$NON-NLS-1$
-                    cid.setItemType(UtilsXML.getChildValue(node.getChildNodes(), "type")); //$NON-NLS-1$
-                    if (cid.getItemId() == null && cid.getItemType() == null) {
-                        throw new Exception(Messages.getString("CaravanManager.1")); //$NON-NLS-1$
-                    }
+        caravanData.setId(id);
+        caravanData.setZone(UtilsXML.getChildValue(caravanNode.getChildNodes(), "zone"));
+        caravanData.setPricePCT(UtilsXML.getChildValue(caravanNode.getChildNodes(), "pricePCT"));
+        caravanData.setCoins(UtilsXML.getChildValue(caravanNode.getChildNodes(), "coins"));
+        caravanData.setBuysString(UtilsXML.getChildValue(caravanNode.getChildNodes(), "buys"));
+        caravanData.setItemList(loadCaravanItems(caravanNode.getChildNodes()));
+        caravanData.setComePCT(UtilsXML.getChildValue(caravanNode.getChildNodes(), "comePCT"));
 
-                    cid.setSpawnChancePercent(UtilsXML.getChildValue(node.getChildNodes(), "PCT")); //$NON-NLS-1$
-                    cid.setQuantity(UtilsXML.getChildValue(node.getChildNodes(), "quantity")); //$NON-NLS-1$
-                    alItemData.add(cid);
-                }
-            }
+        return caravanData;
+    }
+
+    private static void applyOptionalCaravanValues(
+            CaravanManagerItem caravanData,
+            Node caravanNode) throws Exception {
+        NodeList caravanFields = caravanNode.getChildNodes();
+
+        setIfPresent(caravanFields, "zone", caravanData::setZone);
+        setIfPresent(caravanFields, "pricePCT", caravanData::setPricePCT);
+        setIfPresent(caravanFields, "coins", caravanData::setCoins);
+        setIfPresent(caravanFields, "buys", caravanData::setBuysString);
+        setIfPresent(caravanFields, "comePCT", caravanData::setComePCT);
+
+        ArrayList<CaravanItemData> caravanItems = loadCaravanItems(caravanFields);
+        if (caravanItems != null && !caravanItems.isEmpty()) {
+            caravanData.setItemList(caravanItems);
+        }
+    }
+
+    private static void setIfPresent(
+            NodeList caravanFields,
+            String fieldName,
+            CaravanValueSetter applyValue) throws Exception {
+        String fieldValue = UtilsXML.getChildValue(caravanFields, fieldName);
+
+        if (fieldValue != null) {
+            applyValue.set(fieldValue);
+        }
+    }
+
+    private interface CaravanValueSetter {
+        void set(String value) throws Exception;
+    }
+
+    private static void deleteCaravanFromXml(Node deleteNode) {
+        if (deleteNode.getAttributes() == null || deleteNode.getAttributes().getNamedItem("id") == null) {
+            return;
         }
 
-        return alItemData;
+        String idToDelete = deleteNode.getAttributes().getNamedItem("id").getNodeValue();
+
+        if (idToDelete != null) {
+            caravanList.remove(idToDelete);
+        }
+    }
+
+    private static ArrayList<CaravanItemData> loadCaravanItems(NodeList caravanFields) throws Exception {
+        ArrayList<CaravanItemData> itemData = new ArrayList<CaravanItemData>();
+
+        for (int i = 0; i < caravanFields.getLength(); i++) {
+            Node itemNode = caravanFields.item(i);
+
+            if (itemNode.getNodeType() != Node.ELEMENT_NODE) {
+                continue;
+            }
+
+            if (!itemNode.getNodeName().equals("item")) {
+                continue;
+            }
+
+            CaravanItemData caravanItemData = new CaravanItemData();
+            caravanItemData.setItemId(UtilsXML.getChildValue(itemNode.getChildNodes(), "id"));
+            caravanItemData.setItemType(UtilsXML.getChildValue(itemNode.getChildNodes(), "type"));
+
+            if (caravanItemData.getItemId() == null && caravanItemData.getItemType() == null) {
+                throw new Exception(Messages.getString("CaravanManager.1"));
+            }
+
+            caravanItemData.setSpawnChancePercent(UtilsXML.getChildValue(itemNode.getChildNodes(), "PCT"));
+            caravanItemData.setQuantity(UtilsXML.getChildValue(itemNode.getChildNodes(), "quantity"));
+
+            itemData.add(caravanItemData);
+        }
+
+        return itemData;
     }
 
     public static void clear() {
